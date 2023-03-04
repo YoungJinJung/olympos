b5d33faf0214a6f5a2bf3daa05b1b7f32c04b4ca
kuma.jung <yj.jung5078@gmail.com>
2023-03-04 17:07:46 +0900

    modify init db

--- ceres/src/main/resources/db/migration/V1__init.sql
+++ ceres/src/main/resources/db/migration/V1__init.sql
@@ -4,10 +4,15 @@ CREATE TABLE userinfo(
     age INTEGER
 );
 
-INSERT into userinfo(name,age) values ('dax',1);
-INSERT into userinfo(name,age) values ('jupiter',37);
-INSERT into userinfo(name,age) values ('dwkeum',37);
-INSERT into userinfo(name,age) values ('jessy',34);
-INSERT into userinfo(name,age) values ('gslee',32);
-INSERT into userinfo(name,age) values ('zerone',31);
-INSERT into userinfo(name,age) values ('namjsjeong',30);
+INSERT into userinfo(name,age) values ('Abram',1);
+INSERT into userinfo(name,age) values ('Barry',37);
+INSERT into userinfo(name,age) values ('Calvin',37);
+INSERT into userinfo(name,age) values ('Danielle',34);
+INSERT into userinfo(name,age) values ('Eddie',32);
+INSERT into userinfo(name,age) values ('Fernando',31);
+INSERT into userinfo(name,age) values ('Gabrielle',22);
+INSERT into userinfo(name,age) values ('Hadley',55);
+INSERT into userinfo(name,age) values ('Iliana',64);
+INSERT into userinfo(name,age) values ('Jack',31);
+INSERT into userinfo(name,age) values ('Kai',11);
+INSERT into userinfo(name,age) values ('Landon',2);
